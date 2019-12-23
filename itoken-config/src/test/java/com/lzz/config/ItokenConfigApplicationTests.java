f776a7ff99110be519b928a2dfee3071dbd995b9
grcavalier92 <982297627@qq.com>
2019-12-23 09:17:49 +0800

    cinfig

---
+++ itoken-config/src/test/java/com/lzz/config/ItokenConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzz.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItokenConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
